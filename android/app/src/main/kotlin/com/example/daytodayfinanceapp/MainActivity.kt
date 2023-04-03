ee305069c7e7d59d3ca2d61ada35f96c82b9d419
SyedShagufta <sweetyvox123@gmail.com>
2023-04-03 14:36:18 +0530

    login Signup Features

---
+++ android/app/src/main/kotlin/com/example/daytodayfinanceapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.daytodayfinanceapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:daytodayfinanceapp/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'DayToDay Finance App';
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const loginScreen(),
+      ),
+    );
+  }
+}
